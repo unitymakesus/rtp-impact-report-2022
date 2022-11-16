05ecbd73c3ff27e25b5ad41cae7c8a380b4746b9
Andrew Marino <andrew@andrewmarino.net>
2022-11-16 11:17:52 -0500

    add userback script

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ $page->language ?? 'en' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="canonical" href="{{ $page->getUrl() }}">
-        <meta name="description" content="{{ $page->description }}">
-        <title>{{ $page->title }}</title>
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-        <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
-    </head>
-    <body class="text-black bg-slate-100 font-sans antialiased">
-        @yield('body')
-    </body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="canonical" href="{{ $page->getUrl() }}">
+    <meta name="description" content="{{ $page->description }}">
+    <title>{{ $page->title }}</title>
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+    <script>
+      window.Userback = window.Userback || {};
+      Userback.access_token = '10960|75401|G0cCbrFH9G7QCSQUl6rYqBu3e';
+      (function(d) {
+        var s = d.createElement('script');s.async = true;
+        s.src = 'https://static.userback.io/widget/v1.js';
+        (d.head || d.body).appendChild(s);
+      })(document);
+    </script>
+  </head>
+  <body class="text-black bg-slate-100 font-sans antialiased">
+    @yield('body')
+  </body>
 </html>
